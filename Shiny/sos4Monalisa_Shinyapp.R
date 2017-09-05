d23ad66b3c1406fe687b59325a8c15c22397ef5e
Mattia Rossi <mattia.rossi@eurac.edu>
2017-09-05 17:01:59 +0200

    MONAShiny Codewort

--- Shiny/sos4Monalisa_Shinyapp.R
+++ Shiny/sos4Monalisa_Shinyapp.R
@@ -1,10 +1,15 @@
-setwd(paste0(getwd(),"/Shiny/Shiny/"))
-source("DependingFunctions.R")
-source("global.R")
-source("server.R")
-source("ui.R")
 
-shiny<-shinyApp(ui = ui, server=server)
+MonaShiny<-function() {
+  
+  setwd("~/07_Codes/MonalisR/Shiny/Shiny/")
+  source("DependingFunctions.R")
+  source("global.R")
+  source("server.R")
+  source("ui.R")
+  shiny<-shinyApp(ui = ui, server=server)
+  setwd("~/07_Codes/MonalisR")
+  
+  return(shiny)
+} 
 
 
-setwd("~/07_Codes/MonalisR")
