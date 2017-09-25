582b64242214e8aaf12948ad8b2a455d8225d2f3
Mattia Rossi <mattia.rossi@eurac.edu>
2017-09-25 17:10:51 +0200

    Delete Shiny Folder

--- Shiny/DockerSupport/shiny-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Make sure the directory for individual app logs exists
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
-chown shiny.shiny /usr/local/lib/R/site-library
-
-exec shiny-server 2>&1
