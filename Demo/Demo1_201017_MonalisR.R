285e3ffdf740cf94d8827664f708156868ed9293
Mattia Rossi <mattia.rossi@eurac.edu>
2017-10-20 17:28:26 +0200

    Demo Added

--- Demo/Demo1_201017_MonalisR.R
+++ Demo/Demo1_201017_MonalisR.R
@@ -5,7 +5,7 @@
 ####' EURAC Reseach
 
 
-### 1. Load the Package in your R-Environment ----
+### 1. Load the Package----
 
 # Download and load directy from Gitlab
 library(devtools)
@@ -18,10 +18,9 @@ devtools::install_git("https://gitlab.inf.unibz.it/REMSEN/MonalisR",
                       credentials = git2r::cred_user_pass(uname, getPass::getPass()))
 library(MonalisR)
 
-### 2. Take a look at the MonalisR Package functions ----
+### 2. MonalisR Package ----
 
-
-### 3. Checkout the Databases ----
+### 3. Databases ----
 ##* 3.1 MONALISA ----
 
 mnls<-getMonalisaDB()
@@ -30,10 +29,46 @@ mnls3<-getMonalisaDB_sub(subset="station")
 mnls4<- getMonalisaStat()
 mnls4_unique<- unique(getMonalisaStat())
 
-
 s <- "2015-12-31 00:00"
 e <- "2016-02-28 00:00"
-starturl <- setMonalisaURL()
 
-downloadMonalisa(datestart = s,dateend = e)
+mnls_down<-downloadMonalisa(datestart = s,dateend = e)
+
+path<-"C:/Users/MRossi/Documents/07_Codes/"
+downloadMonalisa(datestart = s,dateend = e,path= path,csv=T)
+
+plotMonalisaLeaflet()
+
+plotMonalisaGG(mnls_down[[1]],stat="boxplot")
+
+##* 3.2 Meteo ----
+
+met1<-getMeteoStat()
+met2<-getMeteoStat(format = "spatial")
+met3<-getMeteoSensor()
+met3<-getMeteoSensor(SCODE="27100MS")
+
+s <- "2017-05-01 00:00"
+e <- "2017-06-01 00:00"
+
+a<-downloadMeteo(station_code = "27100MS",
+                 sensor_code = "LT",
+                 datestart = s,
+                 dateend = e)
+
+downloadMeteo(station_code = "27100MS",
+              sensor_code = "LT",
+              datestart = s,
+              dateend = e, path= path,csv=T)
+
+plotMeteoLeaflet()
+
+##* 3.2 Combine them ----
+# The buffmeteo function is able to combine the Points from the MeteoData with other data
+
+library("rgdal")
+shp<- readOGR("C:/Users/MRossi/Documents/07_Codes/03_TestData","TestPoint")
+
+buffmeteo(point=shp,bufferW=10000)
+
 
