d284e71471734b6c59aed23f10c0511ea65400b5
Rossi Mattia <mattia.rossi@eurac.edu>
2018-01-30 16:45:16 +0100

    New MONALISA Get JSOn parser. LTER functional

--- R/MonalisaGet.R
+++ R/MonalisaGet.R
@@ -1,27 +1,28 @@
 #' Set the MONALISA URL
-#' @description Set the MONALISA URL or leave as default.
-#' @param url character; Input the URL to the Timeseries of the MONALISA Database.
+#' @description Set the MONALISA URL or leave as default for EURACs MONALISA Database
+#' @param db character; Input the URL to the Timeseries of the MONALISA Database.
 #' If this parameter is left empty the standard URL will be set.
 #' @export
 
 setMonalisaURL<-function(db=NA){
   
   if(is.na(db)) return("http://monalisasos.eurac.edu/sos/api/v1/timeseries/")
-  else return(url)
+  else return(db)
 }
 
 #' @title Overview of the Monalisa Database
 #' @description This function accesses the Monalisa Database via the API in JSON format
-#' and returns a list of the peatured parameters.
-#' @param url URL; URL of the Monalisa SOS API. If empty a default path is used
+#' and returns a list of the featured parameters.
+#' @param url URL; URL of the Monalisa SOS API. If left empty a default url for the MONALISA database is used.
+#' The url Paramater is needed until the timeseries path (e.g. ".../sos/api/v1/timeseries/")
 #' @param subset character; The subset of interest. If left empty the whole Data frame will be returned. 
-#' Possible Subsets are "foi","procedure","property"
+#' Possible Subsets are "all","id","foi","procedure","property", "combined" and "geom"
 #' @examples 
 #' 
 #' # Get the Information hosted in the MONALISA Database
-#' mnls<-getMonalisaDB()
-#' mnls2<-getMonalisaDB(subset="property")
-#' mnls3<-getMonalisaDB(subset="station")
+#' getMonalisaDB()
+#' getMonalisaDB(subset="property")
+#' getMonalisaDB(subset="station")
 #' 
 #' # END
 #' 
@@ -31,46 +32,46 @@ setMonalisaURL<-function(db=NA){
 #' @importFrom dplyr select
 #' @export
 
-getMonalisaDB<-function(url=NA,subset=""){
+getMonalisaDB<-function(url=NA,subset="all"){
   
+  # Subset NA handling
+  possibleSubsets<-c("all","foi","procedure","property","combined","geom")
+  ie<-is.element(subset,possibleSubsets)
+  if(!isTRUE(ie)) break("The Subset is not available in the function")
+  
+  # Construct URL
   if(is.na(url)){
     url <- setMonalisaURL()
     xmlfile <- jsonlite::fromJSON(url)
   }else{xmlfile <- jsonlite::fromJSON(url)}
   
-  if(subset=="") return(xmlfile)
+  # Normal return when "all" subset
+  if(subset=="all") return(xmlfile)
   
-  split<-str_split(xmlfile$label,",")
+  #Unique Server ID
+  id<-xmlfile$id
+  if(subset=="id") return(id)
   
-  if(subset=="foi"){
-    lsp1<- split %>% 
-      sapply(.,"[[",2) %>% 
-      unique
-    return(lsp1)
-  }
+  # FOI
+  split_1<- foi<- str_split(xmlfile$label,", ") %>% sapply(.,"[[",2)
+  if(subset=="foi") return(unique(split_1))
   
-  if(subset=="procedure"){
-    lsp1<-split %>% 
-      sapply(.,"[[",1)
-      str_split(.," ")
-    lsp2<-lsp1 %>% 
-      grepl(pattern = "_",unlist(.)) %>% 
-      which %>% 
-      unlist(lsp1)[.] %>% 
-      unique
-    return(lsp2)
-  }
+  # Procedure and ObservableProperties
+  split_2<- str_split(xmlfile$label,",") %>% sapply(.,"[[",1)
+  split_3<- str_split(split_2," ")
   
-  if(subset=="property"){
-    lsp1<-split %>% 
-      sapply(.,"[[",1) %>% 
-      gregexpr(pattern =' ',.)
-    lsp2<-lsp1 %>% 
-      lapply(., function(x){x[length(x)]}) %>% 
-      unlist %>% 
-      substring(lsp1,1,.) %>%
-      unique
-    
-    return(lsp2)
-  }
+  proc<-sapply(split_3,function(i) tail(i,n=1L)) 
+  prop<-lapply(split_3,function(i) paste(head(i,n=-1L),collapse=" ")) %>% unlist
+  
+  if(subset=="procedure") return(unique(proc))
+  if(subset=="property")  return(unique(prop))
+  if(subset=="combined")  return(cbind(id,foi,proc,prop))
+  
+  #Geometry
+  geom<-xmlfile$station$geometry$coordinates
+  geomt<-sapply(geom,function(i) head(i,n=-1L)) %>% t
+  geom2<-cbind(split_1,geomt) %>% unique
+  colnames(geom2)<-c("FOI","LAT","LON")
+  if(subset=="geom") return(geom2)
+  
 }
