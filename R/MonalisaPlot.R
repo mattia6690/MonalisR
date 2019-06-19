a9a7ca3a5548e9ebfa14606dda52596e92857b17
mattia6690 <mattia.rossi@eurac.edu>
2019-06-19 14:04:19 +0200

    Added ggplot String evaluation, updated param and import

--- R/MonalisaPlot.R
+++ R/MonalisaPlot.R
@@ -2,22 +2,16 @@
 #' @description Function to spatially plot the MONALISA Stations and the respective FOI
 #' to a Leaflet plot for better visualization.
 #' @param db optional DB table; If empty the standard db file fill be generated
-#' @examples 
-#' 
-#' plotMonalisaLeaflet() # Calls the Already Preconfuigured MONALISA Leaflet
-#' 
-#' # END
-#' 
 #' @import leaflet
-#' @import magrittr
-#' @import tibble
+#' @importFrom magrittr "%>%"
+#' @importFrom tibble as_tibble
 #' @export
 
 plotMonalisaLeaflet<- function(db=NULL){
   
   if(is.null(db)) db<-setMonalisaURL(db=db)
   
-  coords<-getMonalisaDB(db,subset = "geom") %>% as.tibble()
+  coords<-getMonalisaDB(db,subset = "geom") %>% as_tibble()
   
   m<-leaflet() %>% addTiles() %>% addMarkers(
     lng=coords$LAT %>% as.numeric,
@@ -33,33 +27,28 @@ plotMonalisaLeaflet<- function(db=NULL){
 #' @description Plot the MONALISA Requests either as Line or Boxplot. Both outputs
 #' are displayed as GGPlots
 #' @param x tbl, Table with the Value that should be implemented in the
-#' @examples
-#' 
-#' plotMonalisaGG(mnls_down[[1]],stat="boxplot")
-#' 
-#' # END
-#' 
-#' @import magrittr
+#' @param stat chr, You want to plot a "line" (default) or "boxplot"
 #' @import ggplot2
+#' @importFrom dplyr group_by
 #' @export
 
 plotMonalisaGG<-function(x,stat="line"){
   
-  x1<-x %>% names
+  x1<-names(x)
   colnames(x)<-c("Timestamp","values","FOI")
   x$Timestamp<-as.Date(x$Timestamp)
   
   
   if(stat=="line"){
-    g1<-ggplot(data=x,aes(Timestamp,values))+
+    g1<-ggplot(data=x,aes_string("Timestamp","values"))+
     geom_line()+
     ggtitle(paste0("Variablility of ",x1[2]),unique(x$FOI))+
     ylab(x1[2])
   }
   
   if(stat=="boxplot"){
-    x2<-x %>% group_by(Timestamp)
-    g1<-ggplot(x,aes(Timestamp,values,group=Timestamp))+ 
+    x2<-group_by_(x,"Timestamp")
+    g1<-ggplot(x,aes_string("Timestamp","values",group="Timestamp"))+ 
     geom_boxplot()+
     ggtitle(paste0("Variablility of ",x1[2]),unique(x$FOI))+
     ylab(x1[2])
