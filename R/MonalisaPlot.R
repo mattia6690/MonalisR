fae10ff1f6beb095c1a2ec63e1e483b5d5d64109
mattia6690 <mattia.rossi@eurac.edu>
2019-06-19 17:49:52 +0200

    Deleted plotMonalisaGG function

--- R/MonalisaPlot.R
+++ R/MonalisaPlot.R
@@ -9,7 +9,7 @@
 
 plotMonalisaLeaflet<- function(db=NULL){
   
-  if(is.null(db)) db<-setMonalisaURL(db=db)
+  if(is.null(db)) db<-setMonalisaURL()
   
   coords<-getMonalisaDB(db,subset = "geom") %>% as_tibble()
   
@@ -20,40 +20,5 @@ plotMonalisaLeaflet<- function(db=NULL){
   )
   
   return(m)
-  
-}
-
-#' @title Plot MONALISA Request inn GGPlot
-#' @description Plot the MONALISA Requests either as Line or Boxplot. Both outputs
-#' are displayed as GGPlots
-#' @param x tbl, Table with the Value that should be implemented in the
-#' @param stat chr, You want to plot a "line" (default) or "boxplot"
-#' @import ggplot2
-#' @importFrom dplyr group_by
-#' @export
-
-plotMonalisaGG<-function(x,stat="line"){
-  
-  x1<-names(x)
-  colnames(x)<-c("Timestamp","values","FOI")
-  x$Timestamp<-as.Date(x$Timestamp)
-  
-  
-  if(stat=="line"){
-    g1<-ggplot(data=x,aes_string("Timestamp","values"))+
-    geom_line()+
-    ggtitle(paste0("Variablility of ",x1[2]),unique(x$FOI))+
-    ylab(x1[2])
-  }
-  
-  if(stat=="boxplot"){
-    x2<-group_by_(x,"Timestamp")
-    g1<-ggplot(x,aes_string("Timestamp","values",group="Timestamp"))+ 
-    geom_boxplot()+
-    ggtitle(paste0("Variablility of ",x1[2]),unique(x$FOI))+
-    ylab(x1[2])
-  }
-  
-  return(g1)
   
 }
