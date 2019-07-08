12924d64fbb9b36d6484b571220719338c20dfa6
mattia6690 <mattia.rossi@eurac.edu>
2019-07-08 16:22:06 +0200

    importFrom Leaflet

--- R/MonalisaPlot.R
+++ R/MonalisaPlot.R
@@ -2,7 +2,7 @@
 #' @description Function to spatially plot the MONALISA Stations and the respective FOI
 #' to a Leaflet plot for better visualization.
 #' @param db optional DB table; If empty the standard db file fill be generated
-#' @import leaflet
+#' @importFrom leaflet leaflet addTiles addMarkers
 #' @importFrom magrittr "%>%"
 #' @importFrom tibble as_tibble
 #' @export
@@ -11,7 +11,7 @@ plotMonalisaLeaflet<- function(db=NULL){
   
   if(is.null(db)) db<-setMonalisaURL()
   
-  coords<-getMonalisaDB(db,subset = "geom") %>% as_tibble()
+  coords<-getMonalisaDB(db,subset = "geom") %>% as_tibble
   
   m<-leaflet() %>% addTiles() %>% addMarkers(
     lng=coords$LAT %>% as.numeric,
